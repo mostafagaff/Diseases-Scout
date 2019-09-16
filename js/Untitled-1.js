2f15ed2f0e4ba2331400878dfe22c22f13864de0
Mahmoud <mahmoudanwarr10000@gmail.com>
2019-09-16 11:48:08 +0200

    done number 1,2,8

--- js/Untitled-1.js
+++ js/Untitled-1.js
@@ -16,3 +16,17 @@ $(".btnn").click(function () {
     $(".feeee").fadeOut(500);
 
 });
+//$('#outerdiv div:not(#innerdiv2)')
+
+$(".main").css("filter","blur(10px)");
+$(".tutorial1").click(function(){
+    $(".tutorial1").css("display","none");
+    $(".tutorial2").css("display","block");
+    $(".main").css("filter","blur(0px)");
+    $('#one').attr('size',5);
+})
+
+$(".tutorial2").click(function(){
+    $(".tutorial2").css("display","none");
+    $('#one').attr('size',1);
+})
